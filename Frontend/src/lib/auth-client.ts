c247f66a10bd9bb1db105bf4fe3754356621e31e
Mit0921 <meetbhardiwala@gmail.com>
2026-09-01 16:52:30 +0530

    updated auth url in frontend

--- Frontend/src/lib/auth-client.ts
+++ Frontend/src/lib/auth-client.ts
@@ -1,12 +1,9 @@
 // src/lib/auth-client.ts
 import { createAuthClient } from 'better-auth/react';
-import { API_BASE_URL } from './utils';
 
 
 
-export const authClient = createAuthClient({
-    baseURL: API_BASE_URL, // your backend origin
-});
+export const authClient = createAuthClient({});
 
 // Optional convenience exports
 export const { signIn, signUp, signOut, useSession } = authClient;
